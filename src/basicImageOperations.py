da52c8dc9e8bb1750bf42607cfaff03ba9349abf
rajkamaludaya <rajkamaludayasuriyan@gmail.com>
2022-11-04 22:34:21 +0530

    Base commit

---
+++ src/basicImageOperations.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+from dataPath import DATA_PATH
+
+imagePath = DATA_PATH + "/images/img_bw_18x18.png"
+bw_img = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+cv2.imshow("Display Image", bw_img)
+c = cv2.waitKey(5000)
+cv2.destroyAllWindows()
