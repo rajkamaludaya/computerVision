4aa048ee3d368496b404e1043c6e308f12e2cd67
rajkamaludaya <rajkamaludayasuriyan@gmail.com>
2022-11-05 12:23:43 +0530

    Test

--- src/basicImageOperations.py
+++ src/basicImageOperations.py
@@ -7,3 +7,13 @@ bw_img = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
 cv2.imshow("Display Image", bw_img)
 c = cv2.waitKey(5000)
 cv2.destroyAllWindows()
+
+
+imagePath = DATA_PATH + "/images/img_bw_64x64.png"
+bw_img = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+cv2.imshow("Display Image", bw_img)
+c = cv2.waitKey(5000)
+cv2.destroyAllWindows()
+
+
+
