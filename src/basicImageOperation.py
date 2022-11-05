bed753c05b771401812a96fbb6ef2c79479c8fc4
rajkamaludaya <rajkamaludayasuriyan@gmail.com>
2022-11-05 12:56:43 +0530

    Test commit with file name change

--- src/basicImageOperation.py
+++ src/basicImageOperation.py
@@ -5,11 +5,11 @@ from dataPath import DATA_PATH
 imagePath = DATA_PATH + "/images/img_bw_18x18.png"
 bw_img = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
 cv2.imshow("Display Image", bw_img)
-c = cv2.waitKey(5000)
+c = cv2.waitKey(2000)
 cv2.destroyAllWindows()
 
 imagePath = DATA_PATH + "/images/img_bw_64x64.png"
 bw_img = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
 cv2.imshow("Display Image", bw_img)
-c = cv2.waitKey(5000)
+c = cv2.waitKey(2000)
 cv2.destroyAllWindows()
